2ff8632629e4f0c9e12e8364f68b42bf523b7e52
micha <m.scharhag@gmail.com>
2021-12-16 18:08:39 +0100

    initial :-)

---
+++ src/main/java/com/mscharhag/esexample/EsexampleApplication.java
@@ -0,0 +1,13 @@
+package com.mscharhag.esexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mscharhag/esexample/EsexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscharhag.esexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
